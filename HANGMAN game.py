f737ccc2838affa469d0cd4e2df3ce9f0d69d2b2
Mohamed-njikam <66953643+Mohamed-njikam@users.noreply.github.com>
2020-09-07 19:57:34 +0000

    Add files via upload

--- HANGMAN game.py
+++ HANGMAN game.py
@@ -1,25 +1,4 @@
-import random
 
-# Variables for the game
-
-used_letters = set()
-
-english_alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-                    'u', 'v', 'w', 'x', 'y', 'z'}
-
-word_list = ['python', 'java', 'kotlin', 'javascript']
-
-hidden_word = random.choice(word_list)
-
-hidden_word_class = set(hidden_word)
-
-hidden_word_hyphen = "-" * len(hidden_word)  # To create the number of hyphens in the word
-
-hidden_word_hyphen_list = list(hidden_word_hyphen)
-
-hidden_word_hyphen_string = "".join(hidden_word_hyphen_list)
-
-lifes = 8
 
 # The game starts here
 
@@ -28,7 +7,33 @@ print("H A N G M A N ")
 # menu
 
 while True:
-    command = input('Type "play" to play the game, "exit" to quit: ')
+
+    import random
+
+    # Variables for the game
+
+    used_letters = set()
+
+    english_alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                        't',
+                        'u', 'v', 'w', 'x', 'y', 'z'}
+
+    word_list = ['tout', 'ici', 'rien', 'peu', 'chose', 'autre', 'temps', 'vie', 'accord ', 'monde', 'homme', 'veut',
+                 'quelque', 'besoin', 'femme', 'mais', 'comme', 'avant', 'aussi']
+
+    hidden_word = random.choice(word_list)
+
+    hidden_word_class = set(hidden_word)
+
+    hidden_word_hyphen = "-" * len(hidden_word)  # To create the number of hyphens in the word
+
+    hidden_word_hyphen_list = list(hidden_word_hyphen)
+
+    hidden_word_hyphen_string = "".join(hidden_word_hyphen_list)
+
+    lifes = 8
+
+    command = input('\nType "play" to play the game, "exit" to quit: ')
 
     if command == "play":
         while lifes != 0:
@@ -53,8 +58,9 @@ while True:
 
                 if hidden_word_hyphen_string == hidden_word:
                     print(f"""\n{hidden_word_hyphen_string}
-        You guessed the word!
-        You survived!""")
+You guessed the word!
+You survived!""")
+                    hidden_word_hyphen_string = "".join(hidden_word_hyphen_list)
                     break
 
             elif letter not in hidden_word_class:
@@ -63,6 +69,7 @@ while True:
                 lifes -= 1
 
         else:
+            hidden_word_hyphen_string = "".join(hidden_word_hyphen_list)
             print("You are hanged!")
 
     elif command == "exit":
